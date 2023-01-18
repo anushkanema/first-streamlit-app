e68c7393b91ac4450f76d3f580e3a0116d8c3794
anushkanema <76561523+anushkanema@users.noreply.github.com>
2023-01-19 00:38:37 +0530

    added backslash

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit
 
-streamlit.title('My Mom's new healthy Diner')
+streamlit.title('My Mom\'s new healthy Diner')
 streamlit.header('Breakfast Favourites')
 streamlit.text(' 🥣 Omega 3 and Blueberry Oatmeal')
 streamlit.text('  🥗 Kale, & Spinach Rocket Smoothie')
